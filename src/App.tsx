24820cbb2ababf476d037e2fe03aae6398f285d3
Artem-Arutunyan <a.arutunyan92@gmail.com>
2024-07-23 21:52:10 +0300

    project initialization

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        {/* <Route path="/profile" element={<Profile />} />
+        <Route path="/dialogs" element={<Dialogs />} /> */}
+      </Routes>
+    </>
+  );
+}
+
+export default App;
